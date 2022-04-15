abd9bb8f5e71855c15418f90d615045c6466410c
imag0 <78961113+imag0@users.noreply.github.com>
2022-04-15 13:23:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ ESX = null;
 
 emit('esx:getSharedObject', (obj) => ESX = obj)
 
-RegisterCommand("spawn", async (source, args) => {
+RegisterCommand("spawn", (source, args) => {
     let id = args[0]
+    let name = GetPlayerName(source)
     let xPlayer = ESX.GetPlayerFromId(source)
+    let xPlayerek = ESX.GetPlayerFromId(id)
     if (xPlayer.group != 'user') {
-        emitNet("imago:spawnik", id)
-        xPlayer.showNotification('Teleportowales Gracza O Id '+id+' Na Spawna')
+        emit('imagospawn:kk', id, name)
+        xPlayer.showNotification('Teleportowales Gracza O Id '+id+' Na Spawna!')
         return
     }
     xPlayer.showNotification('Nie Masz Permisji')
 })
+
+on('imagospawn:kk'), (id, name) => {
+    xPlayerek.showNotification(`~r~Zostałeś Przeteleportowany Na Spawn Przez ${name} `)
+    const ped = GetPlayerPed(id);
+    SetEntityCoords(ped, -518.3106, -236.856, 35.85, false, false, false, true);
+}
